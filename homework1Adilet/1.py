68afed9427278ed4eb7f8ea451a62be03184f8a8
adilet1515 <adiletbigiyev@gmail.com>
2024-09-15 15:32:46 +0500

    ui zhumysy

---
+++ homework1Adilet/1.py
@@ -0,0 +1,17 @@
+
+for i in range(10):
+    input_year = input('')
+    zhyl = int(input_year)
+    if zhyl == 0:
+        break
+    elif zhyl%4 == 0:
+        print("kibise zhyl")
+    else:
+         print("kibise emes zhyl")
+
+   
+
+
+    
+
+
---
+++ homework1Adilet/2.py
@@ -0,0 +1,11 @@
+for i in range(10):
+    print("san engiz:")
+    input_san = input("")
+    san = int(input_san)
+    if san == 0:
+        break
+    else:
+         print(san**2)
+   
+
+
